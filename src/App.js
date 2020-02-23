03ff95fab5e265be2b91fd384473fcda5a55c076
Zach Jaensch <jaensch11@gmail.com>
2020-02-23 12:02:45 +1100

    basic redux actions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Provider, useDispatch } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore((state = [], action) => [...state, action.add])
+
+function ReduxUpdater() {
+  const dispatch = useDispatch()
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch({ type: 'update', add: 'ddd' })
+        }}
+      >
+        click me
+      </button>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
-    </div>
+    <Provider store={store}>
+      <ReduxUpdater />
+    </Provider>
   );
 }
 
